8eafb0752bf0d39e3c287a43cd9692f6d145b630
Antoine LUCAS <antoinelucas@macbook-pro-de-antoine-2.home>
2018-11-11 15:41:53 +0100

    d

--- heartstone/settings.py
+++ heartstone/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'card',
 ]
 
 MIDDLEWARE = [
@@ -76,12 +77,12 @@ WSGI_APPLICATION = 'heartstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hearthstone',
         'USER': 'test',
         'PASSWORD': 'test',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
