c434202d5cc3f7895124359826b3d6201aa8a929
Diego <waziri.diego@gmail.com>
2018-11-13 17:52:50 +0100

    [FIX] correction templateé

--- card/models.py
+++ card/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class Card(models.Model):
-    name = models.CharField(max_length=100, blank=True)
-    description = models.TextField(max_length=500, blank=True)
-    image = models.CharField(max_length=500, blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    description = models.TextField(max_length=100, blank=True)
+    image = models.CharField(max_length=100, blank=True)
     cost = models.IntegerField()
     attack_point = models.IntegerField()
     life_point = models.IntegerField()
     type_card = models.CharField(max_length=10, blank=True)
     classe = models.CharField(max_length=10, blank=True)
-    rare = models.CharField(max_length=10, blank=True)
+    rare = models.BooleanField(default=False)
     race = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
