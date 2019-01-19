a426a7f63e29034128986a232b207112f94bec97
Antoine LUCAS <antoinelucas@macbook-pro-de-antoine-2.home>
2019-01-19 19:44:48 +0100

    import cartes hearthstone + changements modèles card

--- card/models.py
+++ card/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 class Card(models.Model):
     name = models.CharField(max_length=100, blank=True)
-    description = models.TextField(max_length=500, blank=True)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    cost = models.IntegerField(blank=True, null=True)
+    attack_point = models.IntegerField(blank=True, null=True)
+    life_point = models.IntegerField(blank=True, null=True)
+    type_card = models.CharField(max_length=100, blank=True)
     image = models.CharField(max_length=500, blank=True)
-    cost = models.IntegerField()
-    attack_point = models.IntegerField()
-    life_point = models.IntegerField()
-    type_card = models.CharField(max_length=10, blank=True)
+    imageGold = models.CharField(max_length=500, blank=True)
     classe = models.CharField(max_length=10, blank=True)
-    rare = models.CharField(max_length=10, blank=True)
-    race = models.CharField(max_length=10, blank=True)
+    rare = models.CharField(max_length=10, blank=True, null=True)
+    race = models.CharField(max_length=10, blank=True, null=True)
+    cardSet = models.CharField(max_length=500, blank=True)
+    artist = models.CharField(max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.name
