6528fd044943d4c1059880099d442a4230d6ba6d
Diego <waziri.diego@gmail.com>
2019-01-22 14:17:33 +0100

    [FIX] sell card

---
+++ heartstone/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from store.models import Card_Store, score
+
+def home(request):
+    if request.user :
+        current_user = request.user
+
+    cardDecks = Card_Store.objects.filter(available=1).exclude(user=current_user)
+    point = 0
+
+    return render(request, "home.html", {
+        "cardDecks": cardDecks,
+        "point": point
+    })
+
