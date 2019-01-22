ff89b7a9cb0c3e980a7a11994e994b467d31efe1
Diego <waziri.diego@gmail.com>
2019-01-22 15:52:48 +0100

    [FIX] show store

--- heartstone/views.py
+++ heartstone/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from store.models import Card_Store, score
 
 def home(request):
-    if request.user :
+    point = 0
+
+    if request.user.is_authenticated :
         current_user = request.user
         cardDecks = Card_Store.objects.filter(available=1).exclude(user=current_user)
-        point = 0
         return render(request, "home.html", {
             "cardDecks": cardDecks,
             "point": point
         })
 
-    point = 0
     return render(request, "home.html", {
         "point": point
     })
